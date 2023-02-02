d1d69011f5b0763f5b375a1a49d23166f2475ef4
sakinasyed <104270898+sakinasyed@users.noreply.github.com>
2023-02-02 12:58:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const https = require("https");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  console.log(req.body.cityName);
+  const query = req.body.cityName;
+  const apikey = "32c0c37d54f1a09a8f9e0c678abde6ac";
+  const unit = "metric";
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&appid=" +
+    apikey +
+    "&units=" +
+    unit;
+
+  https.get(url, function (response) {
+    console.log(response.statusCode);
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const description = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.write("<p>The weather is currently" + description + "</p>");
+      res.write(
+        "<h1>The temperature in " +
+          query +
+          " is " +
+          temp +
+          " degrees Celcius.</h1>"
+      );
+      res.write("<img src=" + imgURL + ">");
+      res.send();
+    });
+  });
+});
+
+app.listen(3000, function () {
+  console.log("This server is running on port 3000");
+});
